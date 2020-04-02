d9ad7ec721da258518586dc5423205e2f125d446
Jon Fuller <jonathan.fuller@gmail.com>
2020-04-02 22:22:21 +0200

    Another test

---
+++ app/src/Application.java
@@ -0,0 +1,10 @@
+package me.jonfuller.gradlereleasetest.app;
+
+/**
+ * Some example javadoc
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
